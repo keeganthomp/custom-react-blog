b70c60be56dc251e9c61be368abdbc1602b9a295
keeganthomp <whereiskeegan@gmail.com>
2018-02-18 23:43:33 -0600

    updating images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import juliaAndKeegan from "../images/julia-k-pic.jpg";
+import julia from "../images/j_beach_leather.JPG";
 import "../styles/about.scss";
 
 export default class About extends Component {
   render() {
     return (
       <div className="about">
-        <img className="about-image" src={juliaAndKeegan} alt="" />
+        <img className="about-image" src={julia} alt="" />
         <p className="about-content">
           Some Sentances about who you are, go more in depth here than on the
           first page. This should be a coupe of paragraphs. Keep it light and
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "../styles/homepage.scss";
-// import kAndj from '../images/k-j.jpg'
 
 export default class Homepage extends Component {
   render() {
     return (
       <div>
         <div className="home">
-          <img className='home-image' src={require('../images/k-j.jpg')} alt=""/>
+          <img className='home-image' src={require('../images/j_k_cali.JPG')} alt=""/>
             <p className='home-content'>Some Two or sentence summary about who you are and what you are doing with the blog. Some Two or sentence summary about who you are and what you are doing with the blog. Some Two or sentence summary about who you are and what you are doing with the blog.</p>
         </div>
       </div>
